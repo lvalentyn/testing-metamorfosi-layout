e24d09407f220b35ec58ec6be16934be72b10b15
lvalentyn <valikallstars@gmail.com>
2022-05-19 19:26:41 +0300

    added gsap animation for figures

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ import swiper from "./modules/swiper.js";
 
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all.js";
-import Aos from "aos";
+
 import AOS from "aos/dist/aos.js"
 import $ from "jquery";
 // plugins from npm
@@ -22,12 +22,66 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 	// gsap
 	gsap.registerPlugin(ScrollTrigger);
+	ScrollTrigger.matchMedia({
+		"(min-width: 1024px)": function () {
+			gsap.to('.js-gsap-up', {
+				scrollTrigger: {
+					scrub: 1
+				},
+				y: -200
+			})
+			gsap.to('.js-gsap-down', {
+				scrollTrigger: {
+					scrub: 1
+				},
+				y: 200
+			})
+			gsap.to('.js-gsap-left', {
+				scrollTrigger: {
+					scrub: 1
+				},
+				x: -200
+			})
+			gsap.to('.js-gsap-right', {
+				scrollTrigger: {
+					scrub: 1
+				},
+				x: 200
+			})
+
+			gsap.to('.js-gsap-cross-1', {
+				keyframes: [
+					{ x: '-35vw', y: '-30vh' },
+					{ x: '-75vw', y: '-10vh' },
+					{ x: '-35vw', y: '30vh' },
+					{ x: 0, y: 0 },
+				],
+				duration: 15,
+				rotate: 360,
+				repeat: -1
+			})
+			gsap.to('.js-gsap-cross-2', {
+				keyframes: [
+					{ x: '35vw', y: '-60vh' },
+					{ x: '75vw', y: '10vh' },
+					{ x: '35vw', y: '-30vh' },
+					{ x: 0, y: 0 },
+				],
+				duration: 15,
+				rotate: 360,
+				repeat: -1
+			})
+
+		}
+	})
 
 	ScrollTrigger.create({
 		start: 'top -80',
 		end: 99999,
 		toggleClass: { className: 'sticky', targets: '.js-sticky' }
 	});
+
+
 
 	// my modules
 	smoothScrolling();

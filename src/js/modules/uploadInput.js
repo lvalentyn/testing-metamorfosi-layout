67d2661cca122515614985ebdb1680fc934d1144
lvalentyn <valikallstars@gmail.com>
2022-05-20 10:56:46 +0300

    fixed upload label

--- src/js/modules/uploadInput.js
+++ src/js/modules/uploadInput.js
@@ -1,10 +1,32 @@
 const uploadInput = () => {
 
 	const upload = document.querySelector('.contact-us__upload-input');
+	const uploadLabel = document.querySelector('.contact-us__upload-btn');
+
+	if (upload) {
+		upload.addEventListener('change', function (e) {
+			let fileName = this.files[0].name;
+			const arr = fileName.split('.');
+			dots(arr)
+		})
+
+		upload.addEventListener('drop', (e) => {
+			upload.files = e.dataTransfer.files;
+			const arr = upload.files[0].name.split('.');
+			dots(arr)
+		});
+	}
 
 	['dragenter', 'dragleave', 'dragover', 'drop'].forEach(eventName => {
 		upload.addEventListener(eventName, preventDefaults, false);
 	});
+
+	function dots(arr) {
+		let dots;
+		arr[0].length > 6 ? dots = "..." : dots = '.';
+		const name = arr[0].substring(0, 10) + dots + arr[1];
+		uploadLabel.innerHTML = name
+	}
 
 	function preventDefaults(e) {
 		e.preventDefault();
@@ -26,15 +48,7 @@ const uploadInput = () => {
 		upload.addEventListener(eventName, () => unHighlight(upload), false);
 	});
 
-	upload.addEventListener('drop', (e) => {
-		upload.files = e.dataTransfer.files;
-		console.log(upload.files);
-		let dots;
-		const arr = upload.files[0].name.split('.');
-		arr[0].length > 6 ? dots = "..." : dots = '.';
-		const name = arr[0].substring(0, 10) + dots + arr[1];
-		upload.previousElementSibling.textContent = name;
-	});
+
 };
 
 export default uploadInput;
